4e50eb9dbf55109fd1d59302a05f6a5f0b5d8707
Habibur Rahman <hbrawnak@gmail.com>
2021-09-03 13:07:36 +0600

    Realtime chat fixed with ui

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from . import auth
-from . import login_required
-from . import helper
+from flask_socketio import SocketIO, join_room, emit, send
+
+from . import auth, login_required
+from .helper import get_chat_room_active_users
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = "FvigywQvsF6D2Yc"
+
+socketio = SocketIO(app, manage_session=False)
+room_name = 'chatapp'
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -30,13 +34,13 @@ def home():
 @app.route('/chat', methods=['GET', 'POST'])
 @login_required
 def chat():
-    return render_template('chat.html')
+    return render_template('chat.html', user=auth.get_user_session())
 
 
 @app.route('/info', methods=['GET', 'POST'])
 @login_required
 def info():
-    users = helper.get_chat_room_active_users()
+    users = get_chat_room_active_users()
     return render_template('info.html', users=users)
 
 
@@ -46,6 +50,33 @@ def logout():
     auth.logout()
     return redirect(url_for('home'))
 
+#
+# @socketio.on('join', namespace='/chat')
+# def join(message):
+#     room = room_name
+#     payload = {'user': auth.get_user_session(), 'msg': auth.get_user_session() + ' has entered the room.',
+#                'type': 'status'}
+#     join_room(room)
+#     emit('status', payload, room=room)
+#
+#
+# @socketio.on('text', namespace='/chat')
+# def text(message):
+#     room = room_name
+#     emit('message', {'msg': auth.get_user_session() + ' : ' + message['msg']}, room=room)
+
+
+@app.route('/ping', methods=['POST'])
+def ping():
+    if request.method == 'POST':
+        room = room_name
+        payload = {'user': auth.get_user_session(), 'msg': request.form['msg'], 'type': 'message'}
+        emit('message', payload, room=room, namespace='/chat', broadcast=True)
+        return jsonify({"status": "okay"})
+
+    return jsonify({"status": False})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+    socketio.run(app)
